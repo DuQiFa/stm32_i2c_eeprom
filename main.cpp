8ffffc268f3eb75639c564c0db32f1a0dc206b9a
skyscraper <kwikius@yahoo.com>
2016-04-20 11:17:15 +0100

    updated values in eeprom, readded write to eeprom

--- main.cpp
+++ main.cpp
@@ -17,8 +17,7 @@ bool i2c_read( uint16_t address, uint8_t * data, uint32_t len);
 
 void write_delay()
 {
- auto now = quan::stm32::millis();
-
+   auto now = quan::stm32::millis();
    while ( (quan::stm32::millis() - now) < quan::time_<uint32_t>::ms{100U}){;}
 }
 
@@ -27,10 +26,10 @@ int main()
    setup();
    auto elapsed = quan::stm32::millis();
 
- //  char data[] = {"G234567"};
- //  i2c_write(5U,(uint8_t*)data,8);
+   char data_out[] = {"HloWrld"};
+   i2c_write(5U,(uint8_t const*)data_out,8);
 
- //  write_delay();
+   write_delay();
 
    char data_in[8] = {"-------"};
 
@@ -107,7 +106,6 @@ void prf(const char* text, bool val)
 // address
 bool i2c_read(uint16_t address, uint8_t* data, uint32_t len)
 {
-#if 1
    typedef quan::time_<uint32_t>::ms ms;
 
    if (!test(i2c::is_busy,false,ms{200U},"i2c busy forever")){return false;}
@@ -117,7 +115,7 @@ bool i2c_read(uint16_t address, uint8_t* data, uint32_t len)
    i2c::send_address(eeprom_addr );
 
    if (!test(i2c::get_sr1_addr,true,ms{200U},"no addr set 1")){return false;}
-   (void) i2c::get_sr2_msl();
+   (void) i2c::get_sr2_msl();  
 
    if (!test(i2c::get_sr1_txe,true,ms{200U},"no txe 1")){return false;}
    i2c::send_data( static_cast<uint8_t>((address && 0xFF00) >> 8));
@@ -150,130 +148,7 @@ bool i2c_read(uint16_t address, uint8_t* data, uint32_t len)
    if(! test(i2c::get_stop,true,ms{200U},"couldnt set stop")){return false;}
    serial_port::write("data read ok\n");
    return true;
-   
-#else
-
-   timer_t timer = quan::time_<uint32_t>::ms{200U};
-
-   while (i2c::is_busy()){
-      if ( ! timer()){
-         serial_port::write("i2c busy forever\n");
-         return false;
-      }
-   }
-
-   timer.reset();
-   i2c::set_start(true);
-   while (!i2c::get_sr1_sb() ) {
-      if ( ! timer()){
-         serial_port::write("couldnt get sb\n");
-         return false;
-      }
-   }
-   timer.reset();
-   
-   i2c::send_address(eeprom_addr );
-   while (!i2c::get_sr1_addr() ){
-      if ( ! timer()){
-         serial_port::write("couldnt sel master mode tx\n");
-         return false;
-      }
-   }
-   (void) i2c::get_sr2_msl();
-// eeprom address to read
-   while (! i2c::get_sr1_txe() ){
-      if ( ! timer()){
-         serial_port::write("tx reg never emptied 1\n");
-         prf("tra",i2c::get_sr2_tra());
-         prf("busy",i2c::is_busy());
-         prf("msl",i2c::get_sr2_msl());
-         prf("txe", i2c::get_sr1_txe());
-         return false;
-      }
-   }
-   timer.reset();
-   i2c::send_data( static_cast<uint8_t>((address && 0xFF00) >> 8));
-   while (! i2c::get_sr1_txe() ){
-      if ( ! timer()){
-         serial_port::write("tx reg never emptied 2\n");
-         prf("tra",i2c::get_sr2_tra());
-         prf("busy",i2c::is_busy());
-         prf("msl",i2c::get_sr2_msl());
-         prf("txe", i2c::get_sr1_txe());
-         return false;
-      }
-   }
-   i2c::send_data( static_cast<uint8_t>(address && 0xFF));
-   timer.reset();
-    while (! i2c::get_sr1_txe() ){
-      if ( ! timer()){
-         serial_port::write("tx reg never emptied 2\n");
-         prf("tra",i2c::get_sr2_tra());
-         prf("busy",i2c::is_busy());
-         prf("msl",i2c::get_sr2_msl());
-         prf("txe", i2c::get_sr1_txe());
-         return false;
-      }
-   }
-   while (! i2c::get_sr1_btf() ){
-      if ( ! timer()){
-         serial_port::write("tx reg no btf \n");
-         return false;
-      }
-   }
-   i2c::set_start(true);
-// wait for start
-   while (!i2c::get_sr1_sb() ) {
-      if ( ! timer()){
-         serial_port::write("couldnt get sb\n");
-         return false;
-      }
-   }
-   timer.reset();
-   
-
-   i2c::send_address(eeprom_addr | 1 );
-   while (!i2c::get_sr1_addr() ){
-      if ( ! timer()){
-         serial_port::write("couldnt sel master mode tx\n");
-         return false;
-      }
-   }
-   (void) i2c::get_sr2_msl();
-
-   serial_port::write("got to rx data\n");
-
-   uint32_t bytes_left = len;
-   i2c::enable_ack(true);
-   for ( uint32_t i = 0; i < len; ++i){
-      while (! i2c::get_sr1_rxne() ){
-         if ( ! timer()){
-            serial_port::write("rx reg never empty\n");
-            return false;
-         }
-      }
-      timer.reset();
-      if ( bytes_left == 1){
-         i2c::enable_ack(false);
-         i2c::set_stop(true);
-      }
-      
-      data[i] =i2c::receive_data();
-      --bytes_left;
-     
-      //serial_port::write("read byte\n");
-   }
-   
-   while (i2c::get_stop() ) {
-      if ( ! timer()){
-         serial_port::write("couldnt set stop\n");
-         return false;
-      }
-   }
-   serial_port::write("data read ok\n");
-   return true;
-
-  #endif
+  
 }
 
 
